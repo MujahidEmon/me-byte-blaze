d81cf2896f9686cb2dc5a6d415acf5dc73306e1c
MujahidEmon <mujahidulislam.official@gmail.com>
2024-11-07 12:49:27 +0600

    first

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='text-4xl text-center'>Vite + React</h1>
+     <button className="btn btn-accent">Click</button>
+    </>
+  )
+}
+
+export default App
